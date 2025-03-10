2f030feb06e9e31b6dd42490dc9a76972e1d6e4f
P-H-Pancholi <pancholiphalin@outlook.com>
2025-03-10 21:56:43 +0530

    Migrations

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"strings"
 	"sync/atomic"
 )
 
@@ -36,8 +37,9 @@ func main() {
 	mux.Handle("/app/", cfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	// mux.Handle("/app/", http.FileServer(http.Dir(".")))
 	mux.HandleFunc("GET /api/healthz", HealthHandler)
-	mux.HandleFunc("GET /api/metrics", cfg.NumRequestHandler)
-	mux.HandleFunc("POST /api/reset", cfg.ResetHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.NumRequestHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.ResetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", ValidateChirp)
 
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println(err)
@@ -53,8 +55,16 @@ func HealthHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func (cfg *apiConfig) NumRequestHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	msg := []byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load())))
+	w.Header().Set("Content-Type", "text/html")
+	htmlcontent := fmt.Sprintf(`
+	<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>
+	`, cfg.fileserverHits.Load())
+	msg := []byte(htmlcontent)
 	if _, err := w.Write(msg); err != nil {
 		log.Fatal(err)
 	}
@@ -63,4 +73,69 @@ func (cfg *apiConfig) NumRequestHandler(w http.ResponseWriter, r *http.Request) 
 func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
+}
+
+func ValidateChirp(w http.ResponseWriter, r *http.Request) {
+	chirp := struct {
+		ChirpBody string `json:"body"`
+	}{}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&chirp); err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	if len(chirp.ChirpBody) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+	} else {
+		respondWithJson(w, 200, chirp.ChirpBody)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respBody := struct {
+		ChirpError string `json:"error"`
+	}{
+		ChirpError: msg,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error while marshalling json: %s", err)
+		w.WriteHeader(500)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload string) {
+	respBody := struct {
+		ChirpSuccess bool   `json:"valid"`
+		CleanedBody  string `json:"cleaned_body"`
+	}{
+		ChirpSuccess: true,
+		CleanedBody:  replaceProfane(payload),
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error while marshalling json: %s", err)
+		w.WriteHeader(500)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	w.Write(dat)
+
+}
+
+func replaceProfane(body string) string {
+	words_array := strings.Split(body, " ")
+	for idx, word := range words_array {
+		if strings.ToLower(word) == "kerfuffle" ||
+			strings.ToLower(word) == "sharbert" ||
+			strings.ToLower(word) == "fornax" {
+			words_array[idx] = "****"
+		}
+	}
+	return strings.Join(words_array, " ")
 }
